c600702d294642975f77e9a95de67b982db20bb5
remy.sunride <remy.sunride@gmail.com>
2018-09-13 18:08:25 +0200

    Intinial Commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-layout id="q-app" view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="primary"
+      >
+
+      <!--
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="fas fa-bars"
+        /> -->
+
+        <q-toolbar-title>
+          Départ speed
+          <div slot="subtitle">Mode classique</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <!--
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
+    >
+      <q-list
+        no-border
+        link
+        inset-delimiter
+      >
+        <q-list no-border link inset-delimiter>
+          <q-list-header>Essential Links</q-list-header>
+          <q-item @click.native="openURL('http://quasar-framework.org')">
+            <q-item-side icon="fas fa-graduation-cap" />
+            <q-item-main label="Docs" sublabel="quasar-framework.org"></q-item-main>
+          </q-item>
+          <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
+            <q-item-side icon="fas fa-comments" />
+            <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg"></q-item-main>
+          </q-item>
+          <q-item @click.native="openURL('http://forum.quasar-framework.org')">
+            <q-item-side icon="far fa-clipboard" />
+            <q-item-main label="Forum" sublabel="forum.quasar-framework.org"></q-item-main>
+          </q-item>
+          <q-item @click.native="openURL('https://twitter.com/quasarframework')">
+            <q-item-side icon="fab fa-twitter" />
+            <q-item-main label="Twitter" sublabel="@quasarframework"></q-item-main>
+          </q-item>
+        </q-list>
+      </q-list>
+    </q-layout-drawer>-->
+
+    <q-page-container>
+      <HelloWorld />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'LayoutDefault',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn :loading="loading" @click="simulateProgress" round label="Button Label" size="10vh" icon="fas fa-stopwatch" style="color:#027BE3">
+          <span slot="loading">
+            <q-spinner-gears class="on-left"/>
+          </span>
+    </q-btn>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import {Howl} from 'howler'
+
+export default {
+  
+  data () {
+    return { loading: false }
+  },
+  methods: {
+    
+simulateProgress () {
+      // we set loading state
+      this.loading = true
+      var sound = new Howl({
+      src: ['./marks.mp3']
+      });
+      sound.play()
+
+      // simulate a delay, like in
+      // waiting for an Ajax call
+      setTimeout(() => {
+        // delay is over, now we reset loading state
+      sound = new Howl({
+      src: ['./ready.mp3']
+      });
+      sound.play()
+        var attente = Math.random() * (3 - 1) + 1;
+        setTimeout(()=> {
+
+      sound = new Howl({
+      src: ['./buzzer.mp3']
+      });
+      sound.play()
+          this.loading = false
+        }, attente * 1000)
+        // DON't forget to reset loading state
+        // otherwise the button will keep on
+        // being in "loading" state
+      }, 3000)
+    }
+  }
+}
+</script>
+
